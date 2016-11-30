d386e8e862fc0d9ee507247573ae11b6bd988e4d
Elgolfin <elgo@nicomputer.net>
2016-11-29 20:16:40 -0500

    Solve Problem 002

---
+++ src/Problems/P002.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ProjectEuler.Problems
+{
+    public class P002
+    {
+        public P002()
+        {    
+        }
+
+        public static int SumOfEvenValuesFibonacci (int max)
+        {
+            var previous = 1;
+            var current = 2;
+            var sum = 2;
+            var next = previous + current;
+            while (next < max) {
+                if (next % 2 == 0) {
+                    sum += next;
+                }
+                previous = current;
+                current = next;
+                next = previous + current;
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ test/P002UnitTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Xunit;
+using ProjectEuler.Problems;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ProjectEulerUnitTests
+{
+    public class P002UnitTests
+    {
+        [Fact]
+        public void The_Sum_Of_Even_Values_Under_100_Should_Return_44() 
+        {
+            Assert.Equal(44, P002.SumOfEvenValuesFibonacci(100));
+        }
+        
+        [Fact]
+        public void The_Sum_Of_Even_Values_Under_4000000_Should_Return_4613732() 
+        {
+            Assert.Equal(4613732, P002.SumOfEvenValuesFibonacci(4000000));
+        }
+        
+    }
+}
